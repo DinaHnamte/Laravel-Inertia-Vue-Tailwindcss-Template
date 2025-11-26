dab645b8a48cb5cb38f2163556fb45458fffcb92
DinaHnamte <dinahnamte@gmail.com>
2025-11-27 01:26:30 +0530

    added and configured vue, inertia, tailwind

---
+++ resources/views/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Inertia App</title>
+    @vite(["resources/js/app.js", "resources/css/app.css"])
+    @inertiaHead
+</head>
+
+<body>
+    @inertia
+</body>
+
+</html>
